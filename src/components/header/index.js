b63091795c6cf0e5e58bda34bac2b09df9a7d32a
baaae <reecehudson10@gmail.com>
2019-06-13 16:15:31 -0500

    style using grommet

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,46 +1,55 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import { Box, Heading, Button } from 'grommet';
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
+  <Box
+    as="header"
+    background="neutral-1"
+    pad={{bottom: "small"}}
+    margin={{bottom: "1.45rem"}}
     >
-      <h1 style={{ margin: 0 }}>
+    <Box
+      margin={{vertical: 0, horizontal: "auto"}}
+      width="xlarge"
+      >
+      <Heading
+        level="1"
+        margin="0"
+        >
         <Link
           to="/"
           style={{
-            color: `white`,
-            textDecoration: `none`,
+            color: "white"
           }}
         >
           {siteTitle}
         </Link>
-      </h1>
-      <div>
-        <Link to="/" style={{color: "white"}}>
-          Home
+      </Heading>
+      <Box
+        direction="row"
+        >
+        <Link to="/" style={{color: "white", paddingRight: "10px"}}>
+          <Button label="Home" color="neutral-2"/>
         </Link>
-        <Link to="/contact" style={{color: "white"}}>
-          Contact
+        <Link to="/contact" style={{color: "white", paddingRight: "10px"}}>
+          <Button label="Contact" color="neutral-2"/>
         </Link>
-        <Link to="/about" style={{color: "white"}}>
-          About
+        <Link to="/about" style={{color: "white", paddingRight: "10px"}}>
+          <Button label="About" color="neutral-2"/>
         </Link>
-      </div>
-    </div>
-  </header>
+      </Box>
+    </Box>
+  </Box>
+
+
+
+
 )
+
+
+
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -9,10 +9,10 @@ import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 import { hpe } from 'grommet-theme-hpe';
-import { Grommet } from 'grommet';
+import { Grommet, Box } from 'grommet';
 
 import Header from "../header"
-import "./layout.css"
+import "./style.css"
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -29,21 +29,17 @@ const Layout = ({ children }) => (
       <>
         <Grommet theme={hpe}>
           <Header siteTitle={data.site.siteMetadata.title} />
-          <div
-            style={{
-              margin: `0 auto`,
-              maxWidth: 960,
-              padding: `0px 1.0875rem 1.45rem`,
-              paddingTop: 0,
-            }}
-          >
+          <Box
+            margin={{vertical: "0", horizontal: "auto"}}
+            width="xlarge"
+            >
             <main>{children}</main>
             <footer>
               © {new Date().getFullYear()}, Built with
               {` `}
               <a href="https://www.gatsbyjs.org">Gatsby</a>
             </footer>
-          </div>
+          </Box>
         </Grommet>
       </>
     )}
