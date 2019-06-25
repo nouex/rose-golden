4cec91801d78aa6c4009357998840700df8d5dee
baaae <reecehudson10@gmail.com>
2019-06-25 15:14:59 -0500

    redo complex page

--- src/templates/complex.js
+++ src/templates/complex.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { graphql } from 'gatsby';
-import { Box, Text } from 'grommet';
+import {
+  Box, Text, DataTable, Meter, Heading,
+} from 'grommet';
 import PropTypes from 'prop-types';
 
 import Layout from '../components/layout';
@@ -10,32 +12,95 @@ const Complex = ({ data }) => (
   <Layout>
     <SEO title="Home" keywords={['byui', 'housing', 'students', 'approved']} />
     <Box as="main">
-      {
-        Object.keys(data.postgres.complexById).map((key, ind) => (
-          <Box key={ind}>
-            <Text>
-              {key}
-:
-              {' '}
-              {String(data.postgres.complexById[key])}
-            </Text>
-          </Box>
-        ))
-      }
+      <Box>
+        [photo gallery]
+        <br />
+        ---
+      </Box>
+      <Box>
+        <Box as="span">{data.postgres.complex.name}</Box>
+        <Box as="span">{data.postgres.complex.contact.address}</Box>
+        <Box as="span">{`from: $${data.postgres.complex.minRent}`}</Box>
+        ---
+        <br />
+      </Box>
+      <Box>
+        <Heading level="2">Floorplans</Heading>
+        <DataTable
+          columns={[
+            {
+              property: 'Plan 1',
+              header: <Text>Name</Text>,
+              primary: true,
+            },
+            {
+              property: 'Plan 2',
+              header: 'Complete',
+              render: datum => (
+                <Box pad={{ vertical: 'xsmall' }}>
+                  <Meter
+                    values={[{ value: datum.percent }]}
+                    thickness="small"
+                    size="small"
+                  />
+                </Box>
+              ),
+            },
+          ]}
+          data={[
+            { name: 'Alan', percent: 20 },
+            { name: 'Bryan', percent: 30 },
+            { name: 'Chris', percent: 40 },
+            { name: 'Eric', percent: 80 },
+          ]}
+        />
+      </Box>
+      <Box>
+        <Heading level="3">Amenities</Heading>
+        ...
+      </Box>
+      <Box>
+        <Heading level="3">Special Features</Heading>
+        ...
+      </Box>
+      <Box>
+        <Heading level="3">Community Features</Heading>
+        ...
+      </Box>
+      <Box>
+        [Google Maps goes here]
+      </Box>
     </Box>
   </Layout>
 );
 
 Complex.propTypes = {
-  data: PropTypes.object.isRequired,
+  data: PropTypes.shape({
+    postgres: PropTypes.shape({
+      complex: PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        hasPrivateRoom: PropTypes.bool.isRequired,
+        hasMusicRoom: PropTypes.bool.isRequired,
+        hasWasher: PropTypes.bool.isRequired,
+        isHouse: PropTypes.bool.isRequired,
+        minRent: PropTypes.number.isRequired,
+        maxRent: PropTypes.number.isRequired,
+        contact: PropTypes.shape({
+          id: PropTypes.string.isRequired,
+          address: PropTypes.string.isRequired,
+        }).isRequired,
+      }).isRequired,
+    }).isRequired,
+  }).isRequired,
 };
 
 export default Complex;
 
 export const query = graphql`
-  query getComplexById($id: PostGraphile_UUID!) {
+  query getcomplex($id: PostGraphile_UUID!) {
     postgres {
-      complexById(id: $id) {
+      complex: complexById(id: $id) {
         id
         name
         hasPrivateRoom
@@ -44,6 +109,10 @@ export const query = graphql`
         isHouse
         minRent
         maxRent
+        contact: contactByContact {
+          id
+          address
+        }
       }
     }
   }
