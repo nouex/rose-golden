c16eff6c8913e2eb9e588b4a0841c1a8bb4f4ed9
baaae <reecehudson10@gmail.com>
2019-06-25 14:13:09 -0500

    create .eslintrc

--- src/templates/complex.js
+++ src/templates/complex.js
@@ -1,26 +1,36 @@
-import React from "react"
+import React from 'react';
 import { graphql } from 'gatsby';
 import { Box, Text } from 'grommet';
+import PropTypes from 'prop-types';
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from '../components/layout';
+import SEO from '../components/seo';
 
-export default ({data}) => (
+const Complex = ({ data }) => (
   <Layout>
-    <SEO title="Home" keywords={[`byui`, `housing`, `students`, 'approved']} />
+    <SEO title="Home" keywords={['byui', 'housing', 'students', 'approved']} />
     <Box as="main">
       {
-        Object.keys(data.postgres.complexById).map((key, ind) => {
-          return (
-            <Box key={ind}>
-              <Text>{key}: {String(data.postgres.complexById[key])}</Text>
-            </Box>
-          )
-        })
+        Object.keys(data.postgres.complexById).map((key, ind) => (
+          <Box key={ind}>
+            <Text>
+              {key}
+:
+              {' '}
+              {String(data.postgres.complexById[key])}
+            </Text>
+          </Box>
+        ))
       }
     </Box>
   </Layout>
-)
+);
+
+Complex.propTypes = {
+  data: PropTypes.object.isRequired,
+};
+
+export default Complex;
 
 export const query = graphql`
   query getComplexById($id: PostGraphile_UUID!) {
@@ -37,4 +47,4 @@ export const query = graphql`
       }
     }
   }
-`
+`;
