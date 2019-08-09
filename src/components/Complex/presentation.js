77a3740fb7841ab704adaaf60be4bef7f31d8441
baaae <reecehudson10@gmail.com>
2019-08-09 15:58:37 -0500

    complex card presentational changes

--- src/components/Complex/presentation.js
+++ src/components/Complex/presentation.js
@@ -12,12 +12,15 @@ const ComplexPresentation = ({data, icons}) => {
   return (
     <Box margin={{bottom: "medium"}}>
       <Link to={`/${slug(data.name)}`} style={{height: "100%"}} className="complex-card">
-        <Box pad="none" align="start" border={{all: "xsmall"}} gap="xsmall" width="320px" height="100%" as="article">
+        <Box pad="none" align="start" border={{all: "xsmall"}} gap="xsmall" width="320px" height="100%" as="article" background="light-3">
           <ComplexImage name={data.thumbnail.slug}/>
+          <Box pad="xsmall" width="100%" className="complex-card-title">
+            <Text size="xlarge">{data.name}</Text>
+            <Box as="div" width="100%" height="0" pad={{bottom: "xsmall"}} style={{borderBottom: "1px solid rgba(68, 68, 68, 0.6)"}}></Box>
+          </Box>
           <Box pad="small" width="100%">
             <Box direction="row" justify="between" pad={{bottom: "small"}}>
-              <Text size="large" weight="bold" color="dark-1">{data.rent}</Text>
-              <Text size="xlarge">{data.name}</Text>
+              <Text size="large" color="dark-1">{data.rent}</Text>
             </Box>
             <Box pad={{bottom: "small"}}>
               <Text size="medium">{data.contact.address}</Text>
@@ -27,8 +30,14 @@ const ComplexPresentation = ({data, icons}) => {
              {icons}
             </Box>
             <Box pad={{bottom: "small"}}>
-              <Text size="medium">student capacity <Text weight="bold" color="dark-2">{data.studentCapacity}</Text></Text>
-              <Text size="medium">parking spaces <Text weight="bold" color="dark-2">{data.parkingSpaces}</Text></Text>
+              <Box direction="row" justify="between">
+                <Text size="medium">student capacity</Text>
+                <Text color="dark-2">{data.studentCapacity}</Text>
+              </Box>
+              <Box direction="row" justify="between">
+                <Text size="medium">parking spaces</Text>
+                <Text color="dark-2">{data.parkingSpaces}</Text>
+              </Box>
             </Box>
           </Box>
         </Box>
