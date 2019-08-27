8a5ea53275c5a1d26718b53bcdc7f91f1e1625f5
baaae <reecehudson10@gmail.com>
2019-08-26 23:21:25 -0500

    add google anaylitics with bare minimum config

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,5 +46,12 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // replace "UA-XXXXXXXXX-X" with your own Tracking ID
+        trackingId: "UA-146470940-1",
+      },
+    },
   ],
 };
