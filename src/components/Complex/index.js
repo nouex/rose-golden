9d4c0ded8d4ffc293b831c24bdbdbb7469557cf1
baaae <reecehudson10@gmail.com>
2019-12-18 17:52:35 -0700

    add leniency to amenity scoring

--- src/components/Complex/index.js
+++ src/components/Complex/index.js
@@ -68,7 +68,7 @@ function formatGender(c) {
 
 // TODO: export for testing
 function getAmenityScore(complex, amenities) {
-  const levels = [ "😊", "🙂", "😐", "🙁"/*, "😞"*/ ].reverse()
+  const levels = [ "😊", "🙂", "😐", "🙁", "😞" ].reverse()
   amenities = amenities.filter(amenity => complex[amenity] !== null)
   // const points = amenities.reduce((accum, amenity) => accum + (complex[amenity] === true), 0);
   const points = amenities.filter(amenity => complex[amenity] === true).length
@@ -76,5 +76,8 @@ function getAmenityScore(complex, amenities) {
   const bracketIncrement = base / (levels.length -1) // -1 to make the levels.length exclusive
   const adjustedPoints = +((points / amenities.length) * base).toFixed(0)
   const index = Math.floor(adjustedPoints / bracketIncrement)
-  return levels[index]
+  // NOTE: leniency is how nice we wanna be and up the score, it is a final tweak to the scoring system
+  // that is mostly artificial
+  const leniency = index < levels.length -1 ? 1 : 0
+  return levels[index + leniency]
 }
